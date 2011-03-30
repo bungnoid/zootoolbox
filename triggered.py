1219d0d87ca8d6fd11121139a61c18aebcfff697
Hamish McKenzie <macaronikazoo@gmail.com>
2011-03-30 08:43:35 +0000

    moved command resolution into picker - code is shared between tools
    git-svn-id: https://zootoolbox.googlecode.com/svn/trunk@270 c49f8b0a-e323-0410-bec0-1562a9e7ed1a

--- triggered.py
+++ triggered.py
@@ -7,6 +7,8 @@ import time
 import api
 import maya.cmds as cmd
 
+from picker import resolveCmdStr
+
 mel = api.mel
 melecho = api.melecho
 
@@ -327,55 +329,7 @@ class Trigger(object):
 			try: cmd.deleteAttr( '%s.zooTrig%dcache' % ( self.obj, int(objectOrSlot) ))
 			except TypeError: pass
 	def resolve( self, cmdStr, optionals=[] ):
-		'''
-		returns a resolved cmd string.  the cmd string can be either passed in, or if you specify the slot number
-		the the cmd string will be taken as the given slot's menu command
-		'''
-		cmdStr = str( cmdStr )
-		connects = self.listAllConnectSlots(emptyValue=self.INVALID)
-
-		#if the connects list is empty, early out
-		if not connects: return cmdStr
-
-		#resolve # tokens - these represent self
-		cmdStr = cmdStr.replace( '#', str( self.obj ) )
-
-		#resolve ranged connect array tokens:  @<start>,<end> - these represent what is essentially a list slice - although they're end value inclusive unlike python slices...
-		compile = re.compile
-		arrayRE = compile('(@)([0-9]+),(-*[0-9]+)')
-		def arraySubRep( matchobj ):
-			char,start,end = matchobj.groups()
-			start = int(start)
-			end = int(end) + 1
-			if end == 0: end = None
-			try: return '{ "%s" }' % '","'.join( connects[start:end] )
-			except IndexError: return "<invalid range: %s,%s>" % (start,end)
-
-		cmdStr = arrayRE.sub(arraySubRep,cmdStr)
-
-		#resolve all connect array tokens:  @ - these are represent a mel array for the entire connects array excluding self
-		allConnectsArray = '{ "%s" }' % '","'.join( [con for con in connects[1:] if con != self.INVALID] )
-		cmdStr = cmdStr.replace('@',allConnectsArray)
-
-		#resolve all single connect tokens:  %<x> - these represent single connects
-		connectRE = compile('(%)(-*[0-9]+)')
-		def connectRep( matchobj ):
-			char,idx = matchobj.groups()
-			try: return connects[ int(idx) ]
-			except IndexError: return self.INVALID
-
-		cmdStr = connectRE.sub(connectRep,cmdStr)
-
-		#finally resolve any optional arg list tokens:  %opt<x>%
-		optionalRE = compile('(\%opt)(-*[0-9]+)(\%)')
-		def optionalRep( matchobj ):
-			charA,idx,charB = matchobj.groups()
-			try: return optionals[ int(idx) ]
-			except IndexError: return "<invalid optional>"
-
-		cmdStr = optionalRE.sub(optionalRep,cmdStr)
-
-		return cmdStr
+		return resolveCmdStr( cmdStr, self.obj, self.listAllConnectSlots( emptyValue=self.INVALID ), optionals )
 	def unresolve( self, cmdStr, optionals=[] ):
 		'''given a cmdStr this method will go through it looking to resolve any names into connect tokens.  it only looks for single cmd tokens
 		and optionals - it doesn't attempt to unresolve arrays'''
